ce4b5d1588abdea7b544a2f6846464349ce8fe2b
jobson <jammeloalves@gmail.com>
2022-11-15 08:55:53 -0300

    Inicio projeto financa

---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    darkMode: 'class',
+    theme: {
+      fontFamily: {
+        display: ['Open Sans', 'sans-serif'],
+        body: ['Open Sans', 'sans-serif'],
+      },
+      extend: {
+        fontSize: {
+          14: '14px',
+        },
+        backgroundColor: {
+          'main-bg': '#EEE9E9',
+          'main-dark-bg': '#20232A',
+          'secondary-dark-bg': '#33373E',
+          'light-gray': '#F7F7F7',
+          'half-transparent': 'rgba(0, 0, 0, 0.5)',
+        },
+        borderWidth: {
+          1: '1px',
+        },
+        borderColor: {
+          color: 'rgba(0, 0, 0, 0.1)',
+        },
+        width: {
+          400: '400px',
+          760: '760px',
+          780: '780px',
+          800: '800px',
+          1000: '1000px',
+          1200: '1200px',
+          1400: '1400px',
+        },
+        height: {
+          80: '80px',
+        },
+        minHeight: {
+          590: '590px',
+        },
+        backgroundImage: {
+          'hero-pattern':
+            "url('https://oneflow.com.br/wp-content/uploads/news/startup-managers-presenting-and-analyzing-sales-growth-chart-group-of-workers-with-heap-of-cash-rocket-bar-diagrams-with-arrow-and-heap-of-money.png')",
+        },
+      },
+    },
+    plugins: [],
+  };
